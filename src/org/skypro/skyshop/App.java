9f72aac89ae0fc9ec00d587312a9c1209a8baf2a
Никита <nicki3a@mail.ru>
2025-10-20 14:32:30 +0300

    HW13

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,75 +1,51 @@
 package org.skypro.skyshop;
 
 import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.DiscountedProduct;
+import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.Product;
+import org.skypro.skyshop.product.SimpleProduct;
 
 public class App {
     public static void main(String[] args) {
 
-        Product laptop = new Product("Ноутбук", 50000);
-        Product mouse = new Product("Компьютерная мышь", 2000);
-        Product keyboard = new Product("Клавиатура", 4000);
-        Product monitor = new Product("Монитор", 30000);
-        Product headphones = new Product("Наушники", 5000);
-        Product webcam = new Product("Веб-камера", 6000);
+        SimpleProduct laptop = new SimpleProduct("Ноутбук", 50000);
+        SimpleProduct mouse = new SimpleProduct("Компьютерная мышь", 2000);
+
+        DiscountedProduct keyboard = new DiscountedProduct("Клавиатура", 4000,20);
+        DiscountedProduct monitor = new DiscountedProduct("Монитор", 30000,15);
+
+        FixPriceProduct cable = new FixPriceProduct("Кабель HDMI");
+        FixPriceProduct port = new FixPriceProduct("Display порт");
 
         ProductBasket basket = new ProductBasket();
 
         System.out.println();
-        System.out.println("Работа корзины ");
+        System.out.println(" -Демонстрация обновленной корзины- ");
         System.out.println();
 
-        System.out.println("Добавление товара: ");
         basket.addProduct(laptop);
         basket.addProduct(mouse);
         basket.addProduct(keyboard);
         basket.addProduct(monitor);
-        basket.addProduct(headphones);
-        System.out.println("Добавлено товаров: " + basket.getProductsCount() + " Доступно мест:" + basket.getAvailableSpace());
-        System.out.println("Корзина зполнена: " + basket.isBasketFull());
+        basket.addProduct(cable);
+        basket.addProduct(port);
 
-        System.out.println();
-        System.out.println("Попытка добавить товар в запролненную корзину:");
-        try {
-            basket.addProduct(webcam);
-            System.out.println("Товар добавлен");
-        } catch (IllegalStateException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-            System.out.println("Текущее количество товаров: " + basket.getProductsCount());
-        }
-
-        System.out.println();
-        System.out.println("Содержимое корзины:");
-        System.out.println();
+        System.out.println("Содержиоме корзины:");
         basket.printContents();
+
         System.out.println();
-
-        System.out.println("Общая стоимость: " + basket.getTotalPrice() + " рублей");
+        System.out.println(" -Дополнительны проверки- ");
         System.out.println();
+        System.out.println("Общая стоимсоть: " + basket.getTotalPrice() + " руб.");
+        System.out.println("Количество товаров: " + basket.getProductsCount());
+        System.out.println("Есть ли 'Кабель HDMI' в корзине: " + basket.containsProduct("Кабель HDMI"));
+        System.out.println("Есть ли 'Планшет' в корзине: " + basket.containsProduct("Планшет"));
 
-        System.out.println("Поиск существующего товара: ");
-        System.out.println("Веб-камера в корзине: " + basket.containsProduct("Веб-камера"));
-
-        System.out.println("Поиск существующего товара: ");
-        System.out.println("Компьютерная мышь в корзине: " + basket.containsProduct("Компьютерная мышь"));
         System.out.println();
-
-        System.out.println("Очитска корзины: ");
+        System.out.println(" -Очистка корзины- ");
+        System.out.println();
         basket.clearBasket();
-
-        System.out.println("Содержимое пустой корзины: ");
-        basket.printContents();
-        System.out.println();
-
-        System.out.println("Стоимость пустой корзины: " + basket.getTotalPrice() + " рублей");
-        System.out.println();
-
-        System.out.println("Поиск в пустой корзине: ");
-        System.out.println("Ноутбук в корзине: " + basket.containsProduct("Ноутбук"));
-
-        System.out.println("Добавление товара после очистки: ");
-        basket.addProduct(webcam);
-        System.out.println("Веб-камера успешно добавлена после очистки!");
         basket.printContents();
     }
 }
